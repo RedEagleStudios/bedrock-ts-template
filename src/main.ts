de04e90766e1b43c61d2da3e097743651f513659
ink0rr <tomflynnbeltsazar@gmail.com>
2022-03-15 18:58:06 +0700

    Update dependencies and add examples

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import { AddonGenerator, MCAddon } from "bedrock-ts"
+import { CoreEntity, makeAddon } from "bedrock-ts"
 
-const addon: MCAddon = {
-  packName: "Addon",
-  animControllers: [],
-  animations: [],
-  entities: [],
-  items: [],
-  recipes: [],
+const example: CoreEntity = {
+  identifier: "re:example",
+  createBP(bp) {
+    bp.setComponents({
+      MCTypeFamily: {
+        family: ["example"],
+      },
+      MCCollisionBox: {
+        height: 1,
+        width: 1,
+      },
+      MCPhysics: {},
+      MCPushable: {
+        is_pushable: false,
+      },
+      MCHealth: {
+        value: 1,
+        max: 1,
+      },
+    })
+    return bp.build()
+  },
+  createRP(rp) {
+    return rp.build()
+  },
 }
 
-new AddonGenerator(addon).generate()
+makeAddon({
+  packName: "Example Addon",
+  entities: [example],
+})
